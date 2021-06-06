142930fed00fcb1ea8a02c7513618d8b9acf3390
aschu51 <aeschuma@uci.edu>
2021-06-06 15:41:20 -0700

    Turn Taking

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -275,7 +275,25 @@ socket.on('game_update', (payload) => {
     return;
   }
 
-  $("#my_color").html('<h3 id = "my_color">I am '+my_color+'</h3>');
+  if ( my_color === 'white') {
+    $("#my_color").html('<h3 id = "my_color">I am white</h3>');
+  }
+  else if ( my_color === 'black') {
+    $("#my_color").html('<h3 id = "my_color">I am black</h3>');
+  }
+  else {
+    $("#my_color").html('<h3 id = "my_color">Error I don\'t know what color I am</h3>');
+  }
+
+  if ( payload.game.whose_turn === 'white') {
+    $("#my_color").append('<h4>It is white\'s turn</h4>');
+  }
+  else if ( payload.game.whose_turn === 'black') {
+    $("#my_color").append('<h4>It is black\'s turn</h4>');
+  }
+  else {
+    $("#my_color").append('<h4>Error don\'t know whose turn it is</h4>');
+  }
 
   let whitesum = 0;
   let blacksum = 0;
@@ -381,6 +399,7 @@ socket.on('game_over', (payload) =>{
   }
   if(payload === 'fail'){
     console.log(payload.message);
+    alert(payload.message);
     return;
   }
 
