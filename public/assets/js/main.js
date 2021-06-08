b8c512930892b64a89cac9c2c763e6c23a49da89
aschu51 <aeschuma@uci.edu>
2021-06-07 22:11:58 -0700

    elapsed time updates

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -235,17 +235,18 @@ socket.on('send_chat_message_response', (payload) =>{
 })
 
 let old_board = [
-  ['?', '?', '?', '?', '?', '?', '?', '?'],
-  ['?', '?', '?', '?', '?', '?', '?', '?'],
-  ['?', '?', '?', '?', '?', '?', '?', '?'],
-  ['?', '?', '?', '?', '?', '?', '?', '?'],
-  ['?', '?', '?', '?', '?', '?', '?', '?'],
-  ['?', '?', '?', '?', '?', '?', '?', '?'],
-  ['?', '?', '?', '?', '?', '?', '?', '?'],
-  ['?', '?', '?', '?', '?', '?', '?', '?']
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 ];
 
 let my_color = "";
+let interval_timer;
 
 socket.on('game_update', (payload) => {
   if(( typeof payload == 'undefined') || (payload === null)){
@@ -375,6 +376,29 @@ socket.on('game_update', (payload) => {
       }
     }
   }
+
+  clearInterval(interval_timer)
+  interval_timer = setInterval( ((last_time) => {
+    return ( () => {
+      let d = new Date();
+      let elapsed_m = d.getTime() - last_time;
+      let minutes = Math.floor((elapsed_m / 1000) / 60);
+      let seconds = Math.floor((elapsed_m % (60 * 1000)) / 1000);
+      let total = minutes * 60 + seconds;
+      let timestring = ""+seconds;
+      timestring = timestring.payload(2, '0');
+      timestring = minutes+":"+timestring;
+      if (total > 100) {
+        $("#elapsed").html("Times Up!");
+      }
+      else {
+        $("#elapsed").html(timestring);
+      }
+      $("#elapsed").css("width", total+"%").attr("aria-valuenow", total);
+    })
+  })(payload.game.last_move_time)
+    , 1000);
+
   $("#whitesum").html(whitesum);
   $("#blacksum").html(blacksum);
   old_board = board;
